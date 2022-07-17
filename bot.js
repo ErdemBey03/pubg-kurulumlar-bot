1ec6ae35a4b71ee4ee6d74af35d6ee4829a57117
Erdem <109048333+ErdemBey03@users.noreply.github.com>
2022-07-17 22:09:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,17 @@
-const { 
+const { Telegraf,Telegram  } = require('telegraf')
+
+const config = require("./config")
+
+const telegram = new Telegram(config.token)
+
+const bot = new Telegraf(config.token)
+
+bot.command("start", (ctx) => {
+  ctx.reply("MERHABA, SANA KURULUM KONUSUNDA YARDIM EDECEĞİM İŞTE KOMUTLARIM")
+  }
+            
+    
+            
+            
+console.log("BAŞARILI")
+bot.launch()
